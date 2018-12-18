79d4b3c9bc841dc2dbed82ff5c901aff9fb93a9b
EliasCF <edimbo4@gmail.com>
2018-12-18 18:55:53 +0100

    Basic command-line structure added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct TieperCommand {
+    #[structopt(subcommand)]
+    action: CommandAction,
+}
+
+#[derive(StructOpt, Debug)]
+enum CommandAction {
+    #[structopt(name = "create")]
+    Create {
+        #[structopt(short = "i")]
+        inactive: bool,
+        name: String
+    },
+    #[structopt(name = "remove")]
+    Remove {
+        id: i32
+    },
+    #[structopt(name = "start")]
+    Start {
+        id: i32
+    },
+    #[structopt(name = "stop")]
+    Stop {
+        id: i32
+    },
+    #[structopt(name = "list")]
+    List {
+        #[structopt(short = "a")]
+        active: bool,
+        #[structopt(short = "i")]
+        inactive: bool,
+        name: Option<String>
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = TieperCommand::from_args();
+    println!("{:?}", opt);
 }
